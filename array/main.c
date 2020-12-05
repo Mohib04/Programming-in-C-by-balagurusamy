a7c97e9b2f497405d10b1090029501a2c1c9a59f
Mohibbull Mohib <33421746+Mohib04@users.noreply.github.com>
2020-12-05 10:12:39 +0600

    done

--- array/main.c
+++ array/main.c
@@ -3,6 +3,19 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    //Worked-out problem 7.1
+    //Write a program using a single-subscripted variable to evaluate the following expressions.
+    float number[13];
+    float n,total=0;
+    printf("Enter 10 Real Numbers: \n");
+    for(int i=1; i<=10; i++){
+        scanf("%f", &number[i]);
+    }
+    for(int j=1; j<=10; j++){
+        printf("number[%d] = %.2f \n", j, number[j]);
+        total += number[j];
+    }
+    printf("\nTotal = %f\n", total);
+
     return 0;
 }
