9adbf346d2d7413793ba1df601af2d7df8d8a6ef
Mohibbull Mohib <33421746+Mohib04@users.noreply.github.com>
2020-12-13 16:02:06 +0600

    3.4 problem done

---
+++ operators and expressions/worked-out-problem 3.4/index.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    ///Worked out problem 3.4
+    float a,b,c,x,y,z;
+    a=9;
+    b=12;
+    c=3;
+
+    x= a - b/3 + c*2 - 1;//10
+    y= a - b / (3+c) * (2-1);///7
+    z= a - (b / (3+c) * 2)- 1;///4
+
+    printf("x = %f\n", x);
+    printf("y = %f\n", y);
+    printf("z = %f\n", z);
+}
