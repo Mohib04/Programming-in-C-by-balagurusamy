860124f3dcd7b0df286a6f34c3912abcff4ec6b7
Mohibbull Mohib <33421746+Mohib04@users.noreply.github.com>
2020-12-14 12:48:04 +0600

    some code added

--- operators and expressions/work-out-problem 3.7/index.cpp
+++ operators and expressions/work-out-problem 3.7/index.cpp
@@ -1,16 +1,16 @@
 #include<stdio.h>
-int main(){
     ///Worked-Out Problem 3.7
     ///Type Conversion in expressions
     int n;
     float sum;
-    scanf("%d", &n);
-    for(int i=1; i<=n; i++){
-        sum += 1/(float)i;
-        printf("%2d %6.4f\n", i, sum);
+    int main()
+    {
+        int x = 2, y = 1;
+        x *=x + y;
+        printf("%d\n", x);
+        return 0;;
+        return 0;
     }
 
-
-}
 
 
